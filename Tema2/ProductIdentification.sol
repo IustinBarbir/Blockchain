efdce5f6e36f489f2504f317f8631edb17893e38
Iustin Barbir <126501578+IustinBarbir@users.noreply.github.com>
2024-01-11 18:22:42 +0200

    Update ProductIdentification.sol

--- Tema2/ProductIdentification.sol
+++ Tema2/ProductIdentification.sol
@@ -1,17 +1,14 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-pragma solidity >=0.8.2 <0.9.0;
-
-import "./SampleToken.sol";
+import "./sampleToken.sol"; // Actualizați calea importului
 
 contract ProductIdentification {
     address public owner;
+    address[] public suppliers;
+    mapping(address => uint) public supplierBalances; // Balance of each supplier
 
-    address[] suppliers;
-    Product[] products;
-    uint public registrationFee;
-
-    SampleToken public tokenContract; // Adaugă declarația
+    SampleToken public tokenContract; // Actualizare la tipul de token
 
     struct Product {
         bytes4 id;
@@ -20,83 +17,81 @@ contract ProductIdentification {
         address owner;
     }
 
-    constructor(uint _registrationFee, address _tokenContractAddress) {
+    mapping(bytes4 => Product) public products;
+
+    uint public registrationFee;
+
+    constructor(uint _registrationFee, address _tokenContractAddress) payable  {
         owner = msg.sender;
         registrationFee = _registrationFee;
-        tokenContract = SampleToken(_tokenContractAddress); // Instantierea
+        tokenContract = SampleToken(_tokenContractAddress); // Instantiate the token contract
     }
 
-    function registerSuppliers() public payable {
-        require(!supplierIsRegistered(), "Supplier is already registered");
-        require(msg.value >= registrationFee, "Value must be greater than 0");
-
-        uint change = msg.value - registrationFee;
-
-        suppliers.push(msg.sender);
-
-        // Actualizare pentru a utiliza transferFrom
-        require(tokenContract.transferFrom(owner, msg.sender, registrationFee), "Token transfer failed");
-
-        if (change != 0) {
-            payable(msg.sender).transfer(uint(change));
-        }
-    }
-
-    function registerProduct(string memory _name, uint _amount) public {
-        require(supplierIsRegistered(), "Supplier is not registered");
-
-        bytes4 _id = bytes4(keccak256(abi.encodePacked(msg.sender, _name)));
-        require(!productExist(_id), "Product is already registered");
-
-        products.push(Product(_id, _name, _amount, msg.sender));
-    }
-
-    modifier only_registered_supplier() {
-        require(supplierIsRegistered(), "Supplier is not registered");
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
         _;
     }
 
-    function updateRegistrationFee(uint _newRegistrationFee) external only_owner {
-        registrationFee = _newRegistrationFee;
+    function registerSuppliers() external payable {
+        require(!supplierIsRegistered(), "Supplier is already registered");
+        require(msg.value >= registrationFee, "Value must be greater than or equal to the registration fee");
+
+        uint change = msg.value - registrationFee;
+        
+        suppliers.push(msg.sender);
+        supplierBalances[msg.sender] += registrationFee;
+
+        if (change != 0) {
+            payable(msg.sender).transfer(change);
+        }
+
+        // Transfer registration fee in tokens to owner
+        require(tokenContract.transferFrom(msg.sender, owner, registrationFee), "Token transfer failed");
     }
 
-    function getProductCount() public view returns (uint) {
-        return products.length;
+    function registerProduct(string memory _name, uint _amount) external {
+        require(supplierIsRegistered(), "Supplier is not registered");
+        
+        bytes4 _id = bytes4(keccak256(abi.encodePacked(msg.sender, _name)));
+        require(!productExist(_id), "Product is already registered");
+
+        products[_id] = Product(_id, _name, _amount, msg.sender);
     }
 
-    function getAllProducts() public view returns (Product[] memory) {
-        return products;
-    }
-
-    function getProductsBySupplier(address _supplier) public view returns (Product[] memory) {
-        Product[] memory supplierProducts;
-        for (uint i = 0; i < products.length; i++) {
-            if (products[i].owner == _supplier) {
-                supplierProducts.push(products[i]);
+    function supplierIsRegistered() public view returns (bool) {
+        for (uint i = 0; i < suppliers.length; i++) {
+            if (suppliers[i] == msg.sender) {
+                return true;
             }
         }
-        return supplierProducts;
+
+        return false;
     }
 
-    function getProductById(bytes4 _id) public view returns (Product memory) {
-        for (uint i = 0; i < products.length; i++) {
-            if (products[i].id == _id) {
-                return products[i];
-            }
-        }
-        revert("Product not found");
+    function productExist(bytes4 _id) public view returns (bool) {
+        return products[_id].id != 0;
     }
 
-    function updateProductQuantity(bytes4 _id, int _quantity) external only_registered_supplier {
-        for (uint i = 0; i < products.length; i++) {
-            if (products[i].id == _id) {
-                // Actualizare cantitate folosind transferFrom
-                require(tokenContract.transferFrom(products[i].owner, msg.sender, uint(_quantity)), "Token transfer failed");
-                products[i].volume = uint(int(products[i].volume) + _quantity);
-            }
-        }
+    function getProductInformation(bytes4 _id) external view returns (bytes4, string memory, uint, address) {
+        require(productExist(_id), "Product doesn't exist");
+        Product memory product = products[_id];
+        return (product.id, product.name, product.volume, product.owner);
     }
 
-    // Eveniment pentru înregistrarea unui produs
-    event ProductRegisteredEvent(bytes4 productId, string name, uint volume, address indexed owner);
+    function updateQuantity(bytes4 _id, int _quantity) external {
+        require(supplierIsRegistered(), "Supplier is not registered");
+        require(productExist(_id), "Product doesn't exist");
+
+        products[_id].volume = uint(int(products[_id].volume) + _quantity);
+    }
+
+    // Function to withdraw supplier's balance
+    function withdrawBalance() external {
+        require(supplierIsRegistered(), "Supplier is not registered");
+        uint balance = supplierBalances[msg.sender];
+        require(balance > 0, "No balance to withdraw");
+
+        supplierBalances[msg.sender] = 0;
+        require(tokenContract.transfer(msg.sender, balance), "Token transfer failed");
+    }
 }

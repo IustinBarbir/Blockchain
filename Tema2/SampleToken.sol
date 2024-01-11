eade8e922b8bc6cd90cdd6f2d6e671ec63cb7263
Iustin Barbir <126501578+IustinBarbir@users.noreply.github.com>
2024-01-11 15:33:51 +0200

    Update SampleToken.sol

--- Tema2/SampleToken.sol
+++ Tema2/SampleToken.sol
@@ -3,8 +3,8 @@
 pragma solidity ^0.8.0;
 
 contract SampleToken {
-    string public tokenName = "Sample Token";
-    string public tokenSymbol = "TOK";
+    string public name = "Sample Token";
+    string public symbol = "TOK";
     uint256 public totalSupply;
 
     mapping (address => uint256) public balanceOf;
@@ -19,8 +19,20 @@ contract SampleToken {
         emit Transfer(address(0), msg.sender, totalSupply);
     }
 
+    function name() public view returns (string memory) {
+        return name;
+    }
+
+    function symbol() public view returns (string memory) {
+        return symbol;
+    }
+
     function decimals() public view returns (uint8) {
-        return 18;
+        return 18; // Set the number of decimals as needed
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return totalSupply;
     }
 
     function transfer(address _to, uint256 _value) public returns (bool success) {
@@ -50,39 +62,5 @@ contract SampleToken {
 
         emit Transfer(_from, _to, _value);
         return true;
-    }
-}
-
-contract SampleTokenSale {
-    address public tokenOwner;
-    uint256 public salePrice;
-
-    event SalePriceUpdated(uint256 newPrice);
-
-    modifier onlyTokenOwner() {
-        require(msg.sender == tokenOwner, "Only token owner can call this function");
-        _;
-    }
-
-    constructor(address _token, uint256 _initialSalePrice) {
-        tokenOwner = msg.sender;
-        salePrice = _initialSalePrice;
-    }
-
-    function setSalePrice(uint256 _newPrice) public onlyTokenOwner {
-        salePrice = _newPrice;
-        emit SalePriceUpdated(_newPrice);
-    }
-
-    function purchaseTokens(uint256 _amount) public payable {
-        require(msg.value >= salePrice * _amount, "Insufficient payment");
-
-        SampleToken tokenContract = SampleToken(msg.sender);
-        require(tokenContract.transferFrom(tokenOwner, msg.sender, _amount), "Token transfer failed");
-
-        // Return any excess payment to the buyer
-        if (msg.value > salePrice * _amount) {
-            payable(msg.sender).transfer(msg.value - salePrice * _amount);
-        }
     }
 }
